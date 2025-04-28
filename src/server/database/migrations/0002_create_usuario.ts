df6144101dfeb821b960883b73895179c07b16ea
Ivan <contaivanrodrigues@gmail.com>
2025-04-28 16:37:03 -0300

    feat: Created providers for usuarios

--- src/server/database/migrations/0002_create_usuario.ts
+++ src/server/database/migrations/0002_create_usuario.ts
@@ -6,9 +6,9 @@ export async function up(knex: Knex) {
     return knex.schema
         .createTable(ETableNames.usuario, (table) => {
             table.bigIncrements("id").primary().index();
-            table.string("nome").notNullable().checkLength('>',  3);
-            table.string("senha").notNullable().checkLength('>',  5);
-            table.string("email").index().unique().notNullable().checkLength('>',  6);
+            table.string("nome").notNullable().checkLength('>=',  3);
+            table.string("senha").notNullable().checkLength('>=',  5);
+            table.string("email").index().unique().notNullable().checkLength('>=',  6);
             
             table.comment("Tabela usada para armazenar pessoas do sistema.");
         })
