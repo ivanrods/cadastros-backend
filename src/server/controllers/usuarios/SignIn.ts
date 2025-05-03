74efd446adc36ff76c532ba7d5c40681d3c396a8
Ivan <contaivanrodrigues@gmail.com>
2025-05-03 16:39:59 -0300

    feat: added jwt generation and updated middleware and controllers

--- src/server/controllers/usuarios/SignIn.ts
+++ src/server/controllers/usuarios/SignIn.ts
@@ -1,43 +1,58 @@
-import { Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
-import * as yup from 'yup';
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
-import { UsuariosProvider } from '../../database/providers/usuarios';
-import { validation } from '../../shared/middleware';
-import { IUsuario } from '../../database/models';
-import { PasswordCrypto } from '../../shared/services';
+import { UsuariosProvider } from "../../database/providers/usuarios";
+import { JWTService, PasswordCrypto } from "../../shared/services";
+import { validation } from "../../shared/middleware";
+import { IUsuario } from "../../database/models";
 
-
-interface IBodyProps extends Omit<IUsuario, 'id' | 'nome'> { }
+interface IBodyProps extends Omit<IUsuario, "id" | "nome"> {}
 
 export const signInValidation = validation((getSchema) => ({
-  body: getSchema<IBodyProps>(yup.object().shape({
-    senha: yup.string().required().min(6),
-    email: yup.string().required().email().min(5),
-  })),
+    body: getSchema<IBodyProps>(
+        yup.object().shape({
+            senha: yup.string().required().min(6),
+            email: yup.string().required().email().min(5),
+        })
+    ),
 }));
 
-export const signIn = async (req: Request<{}, {}, IBodyProps>, res: Response) => {
-  const { email, senha } = req.body;
+export const signIn = async (
+    req: Request<{}, {}, IBodyProps>,
+    res: Response
+) => {
+    const { email, senha } = req.body;
 
+    const usuario = await UsuariosProvider.getByEmail(email);
+    if (usuario instanceof Error) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            errors: {
+                default: "Email ou senha são inválidos",
+            },
+        });
+    }
 
-  const result = await UsuariosProvider.getByEmail(email);
-  if (result instanceof Error) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      errors: {
-        default: 'Email ou senha são inválidos'
-      }
-    });
-  }
+    const passwordMatch = await PasswordCrypto.verifyPassword(
+        senha,
+        usuario.senha
+    );
+    if (!passwordMatch) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({
+            errors: {
+                default: "Email ou senha são inválidos",
+            },
+        });
+    } else {
+        const accessToken = JWTService.sign({ uid: usuario.id });
+        if (accessToken === "JWT_SECRET_NOT_FOUND") {
+            return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                errors: {
+                    default: "Erro ao gerar o token de acesso",
+                },
+            });
+        }
 
-  const passwordMatch = PasswordCrypto.verifyPassword(senha, result.senha)
-  if (!passwordMatch) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      errors: {
-        default: 'Email ou senha são inválidos'
-      }
-    });
-  } else {
-    return res.status(StatusCodes.OK).json({ accessToken: 'teste.teste.teste' });
-  }
+        return res.status(StatusCodes.OK).json({ accessToken });
+    }
 };
