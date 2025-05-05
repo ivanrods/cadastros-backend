6df9ad8bdbd1ca2070af41dc16dee8c12ab3a6dd
Ivan <contaivanrodrigues@gmail.com>
2025-05-05 16:07:14 -0300

    feat: added environment config for production

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -1,5 +1,12 @@
 import { Knex } from "knex";
 import path from "path";
+import pg from "pg";
+
+import 'dotenv/config'
+
+if (process.env.NODE_ENV === "production") {
+    pg.types.setTypeParser(20, "text", parseInt);
+}
 
 export const development: Knex.Config = {
     client: "sqlite3",
@@ -34,5 +41,21 @@ export const test: Knex.Config = {
 };
 
 export const production: Knex.Config = {
-    ...development,
+    client: "pg",
+
+    migrations: {
+        directory: path.resolve(__dirname, "..", "migrations"),
+    },
+    seeds: {
+        directory: path.resolve(__dirname, "..", "seeds"),
+    },
+
+    connection: {
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        database: process.env.DATABASE_NAME,
+        password: process.env.DATABASE_PASSWORD,
+        port: Number(process.env.DATABASE_PORT || 5432),
+        ssl: { rejectUnauthorized: false },
+    },
 };
