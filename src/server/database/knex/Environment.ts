b51ced8d5c12f434d5628c342af40b1aca3b6b03
Ivan <contaivanrodrigues@gmail.com>
2025-05-18 10:37:35 -0300

    fix: environment for production

--- src/server/database/knex/Environment.ts
+++ src/server/database/knex/Environment.ts
@@ -35,18 +35,18 @@ export const test: Knex.Config = {
 
 export const production: Knex.Config = {
     client: "pg",
-    migrations: {
-        directory: path.resolve(__dirname, "..", "migrations"),
-    },
-    seeds: {
-        directory: path.resolve(__dirname, "..", "seeds"),
-    },
-    connection: {
+    connection: process.env.DATABASE_URL || {
         host: process.env.DATABASE_HOST,
         user: process.env.DATABASE_USER,
         database: process.env.DATABASE_NAME,
         password: process.env.DATABASE_PASSWORD,
         port: Number(process.env.DATABASE_PORT || 5432),
         ssl: { rejectUnauthorized: false },
+    },
+    migrations: {
+        directory: path.resolve(__dirname, "..", "migrations"),
+    },
+    seeds: {
+        directory: path.resolve(__dirname, "..", "seeds"),
     },
 };
